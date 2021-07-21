1055598e4e4b2560f10f266f3feff86cee307989
VolovatovTA <tima929_v@rambler.ru>
2021-07-21 17:51:27 +0300

    yeeeeeees! VerticalSeecBar implemented and almost correctly work!)))

--- app/src/main/java/com/example/myapplication/VerticalSeekBar.java
+++ app/src/main/java/com/example/myapplication/VerticalSeekBar.java
@@ -1,12 +1,13 @@
+package com.example.myapplication;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.widget.SeekBar;
 
-public class VerticalSeekBar extends SeekBar {
+public class VerticalSeekBar extends androidx.appcompat.widget.AppCompatSeekBar {
 
     public VerticalSeekBar(Context context) {
         super(context);
@@ -32,7 +33,7 @@ public class VerticalSeekBar extends SeekBar {
 
     protected void onDraw(Canvas c) {
         c.rotate(-90);
-        c.translate(-getHeight(),0);
+        c.translate(-getHeight(), 0);
 
         super.onDraw(c);
     }
@@ -47,17 +48,33 @@ public class VerticalSeekBar extends SeekBar {
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_MOVE:
             case MotionEvent.ACTION_UP:
-                int i=0;
-                i=getMax() - (int) (getMax() * event.getY() / getHeight());
+                int i = 0;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    i = getMax() - (int) ((getMax() - getMin()) * event.getY() / getHeight());
+
+                    Log.i("Progress", "getMax = " + getMax() + "");
+                    Log.i("Progress", "getMin = " + getMin() + "");
+                    Log.i("Progress", "event.getY = " + event.getY() + "");
+                    Log.i("Progress", "getHeight = " + getHeight() + "");
+                }
+
+                else i = getMax() - (int) (getMax() * event.getY() / getHeight());
+
                 setProgress(i);
-                Log.i("Progress",getProgress()+"");
+                Log.i("Progress", getProgress() + "");
                 onSizeChanged(getWidth(), getHeight(), 0, 0);
+
                 break;
 
             case MotionEvent.ACTION_CANCEL:
                 break;
         }
         return true;
+    }
+
+    @Override
+    public synchronized void setProgress(int progress) {
+        super.setProgress(progress);
     }
 
 }
