5869854cecc2f667896a5f867a509d026d2dfaf3
VolovatovTA <tima929_v@rambler.ru>
2021-08-28 07:28:01 +0300

    поработал немного и схранил

--- app/src/main/java/com/example/myapplication/UserModel_Track.java
+++ app/src/main/java/com/example/myapplication/UserModel_Track.java
@@ -18,53 +18,41 @@ public class UserModel_Track {
     public int count_of_tracks;
 
 
-    public UserModel_Track(){
+    public UserModel_Track() {
 
     }
+
     ArrayList<Track> tracks = null;
-    public ArrayList<Track> getTracks(Context context){
+
+    public ArrayList<Track> getTracks(Context context) {
         dbHelper = new DBHelper(context);
-        if(database ==null)
-
-    {
-        database = (dbHelper).getWritableDatabase();
-    }
-
-    cursor = database.query(DBHelper.TABLE_TRACKS,null,null,null,null,null,null);
-
-        if(cursor.getCount() != 0)
-    {
-        count_of_tracks = cursor.getCount();
-        String[] names = new String[count_of_tracks];
-        boolean[] acc = new boolean[count_of_tracks];
-        int[] count1 = new int[count_of_tracks];
-        int[]count2 = new int[count_of_tracks];
-        int[]temp = new int[count_of_tracks];
-        int[] ides = new int[count_of_tracks];
-        int[] positions = new int[count_of_tracks];
-        cursor.moveToFirst();
-        int j = 0;
-
-        tracks = new ArrayList<>(cursor.getCount());
-
-        do {
-            names[j] = cursor.getString(cursor.getColumnIndex(DBHelper.KEY_NAME));
-            temp[j] = cursor.getInt(cursor.getColumnIndex(DBHelper.KEY_TEMP));
-            acc[j] = cursor.getInt(cursor.getColumnIndex(DBHelper.KEY_ACCENT)) != 0;
-            count1[j] = cursor.getInt(cursor.getColumnIndex(DBHelper.KEY_COUNT1));
-            count2[j] = cursor.getInt(cursor.getColumnIndex(DBHelper.KEY_COUNT2));
-            ides[j] = cursor.getInt(cursor.getColumnIndex(DBHelper.KEY_ID));
-            positions[j] = cursor.getInt(cursor.getColumnIndex(DBHelper.KEY_POSITION));
-            tracks.add(new Track(names[j], temp[j], acc[j], count1[j], count2[j], positions[j], ides[j]));
-            j++;
-        } while (cursor.moveToNext());
-
-    }
-        else {
-            count_of_tracks = 0;
+        if (database == null) {
+            database = (dbHelper).getWritableDatabase();
         }
+
+        cursor = database.query(DBHelper.TABLE_TRACKS, null, null, null, null, null, null);
+
+        if (cursor.getCount() != 0) {
+            cursor.moveToFirst();
+
+            tracks = new ArrayList<>(cursor.getCount());
+
+            do {
+
+                tracks.add(new Track(cursor.getString(cursor.getColumnIndex(DBHelper.KEY_NAME)),
+                        cursor.getInt(cursor.getColumnIndex(DBHelper.KEY_TEMP)),
+                        cursor.getInt(cursor.getColumnIndex(DBHelper.KEY_ACCENT)) != 0,
+                        cursor.getInt(cursor.getColumnIndex(DBHelper.KEY_COUNT1)),
+                        cursor.getInt(cursor.getColumnIndex(DBHelper.KEY_COUNT2)),
+                        cursor.getInt(cursor.getColumnIndex(DBHelper.KEY_POSITION)),
+                        cursor.getInt(cursor.getColumnIndex(DBHelper.KEY_ID))));
+            } while (cursor.moveToNext());
+
+        }
+        count_of_tracks = cursor.getCount();
+
         return tracks;
-}
+    }
 
     public void deleteItemById(int position) {
         tracks.remove(position);
@@ -82,7 +70,8 @@ public class UserModel_Track {
         database.update(DBHelper.TABLE_TRACKS, cv_to, " position = ?", new String[]{fromPos + ""});
         database.update(DBHelper.TABLE_TRACKS, cv_from, " position = ?", new String[]{toPos + ""});
     }
-    public void close(){
+
+    public void close() {
         database.close();
 
     }
@@ -90,11 +79,22 @@ public class UserModel_Track {
     public void putTrack(Track track) {
         ContentValues contentValues = new ContentValues();
 
-        contentValues.put(DBHelper.KEY_NAME, name_of_track);
-        contentValues.put(DBHelper.KEY_TEMP, (int) freq);
 
-        contentValues.put(DBHelper.KEY_COUNT1, count1);
-        contentValues.put(DBHelper.KEY_COUNT2, count2);
-        
+        contentValues.put(DBHelper.KEY_NAME, track.name);
+        contentValues.put(DBHelper.KEY_TEMP, track.temp);
+        int acc;
+        if (!track.acc) {
+            acc = 0;
+        }
+        else {
+            acc = 1;
+        }
+        contentValues.put(DBHelper.KEY_ACCENT, acc);
+        contentValues.put(DBHelper.KEY_COUNT1, track.count1);
+        contentValues.put(DBHelper.KEY_COUNT2, track.count2);
+        contentValues.put(DBHelper.KEY_ID, cursor.getCount());
+        contentValues.put(DBHelper.KEY_POSITION, cursor.getCount());
+
+        tracks.add(track);
     }
 }
